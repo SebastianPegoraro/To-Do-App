e0eded9159faa34087d2b1e25be1a895ff6953db
SebastianPegoraro <sebapego18@gmail.com>
2020-11-17 22:38:10 -0300

    Empezando

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#app",
+    data: {
+      todos: [
+        { text: 'todo 1', done: false, id: Date.now() },
+        { text: 'todo 2', done: false, id: Date.now() + 1 }
+      ]
+    },
+    methods: {
+  
+    }
+  })
